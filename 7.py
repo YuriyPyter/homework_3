019a3c6eec271922a4acf8587afdc82891c9d7f1
YuriyPyter <jurigruzdov@yandex.ru>
2021-11-29 21:57:45 +0300

    a

--- 7.py
+++
@@ -1,9 +0,0 @@
-def int_func():
-    my_words = input('Введите слова из маленьких латинских букв: ').split()
-    my_list = []
-    for el in my_words:
-        my_list.append(el.title())
-    print(my_list)
-
-
-int_func()
